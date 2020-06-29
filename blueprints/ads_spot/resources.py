0602d4e5fd4944bca4557574d580bfe04390dfe3
Bagas17k <kurniajati@alterra.id>
2020-06-29 19:33:28 +0700

    edit all warns

--- blueprints/ads_spot/resources.py
+++ blueprints/ads_spot/resources.py
@@ -60,7 +60,7 @@ class AdsSpotResource(Resource):
         qry = AdsSpots.query.get(id)
         QRY = marshal(qry, AdsSpots.response_fields)
         image = AdsImages.query.filter_by(ads_spot_id=id)
-        kategori = ProductTypes.query.filter_by(id=qry.product_type_id).first()
+        kategori = ProductTypes.query.filter_by(id=QRY["product_type_id"]).first()
         rows = []
         for row in image.all():
             rows.append(marshal(row, AdsImages.response_field))
